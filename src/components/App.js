b4813860ced938d649bea7000b833047a60b92a8
adam allalou <aabou9@gmail.com>
2021-02-23 11:44:13 +0100

    add all buttons

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ import Display from './Display';
 function App() {
   return (
     <div className="App">
-      Calculator app:
       <Display count="20.0" />
+
       <ButtonPanel />
     </div>
   );
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,7 +3,33 @@ import Button from './Button';
 
 const ButtonPanel = () => (
   <div>
-    <Button name="hey" />
+    <div className="G1">
+      <Button name="AC" />
+      <Button name="+/-" />
+      <Button name="%" />
+      <Button name="/" />
+    </div>
+
+    <div className="G2">
+      <Button name="7" />
+      <Button name="8" />
+      <Button name="9" />
+      <Button name="*" />
+    </div>
+
+    <div className="G3">
+      <Button name="4" />
+      <Button name="5" />
+      <Button name="6" />
+      <Button name="-" />
+    </div>
+
+    <div className="G4">
+      <Button name="3" />
+      <Button name="2" />
+      <Button name="1" />
+      <Button name="+" />
+    </div>
   </div>
 );
 
