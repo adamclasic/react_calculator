7794f8fc541a6bc92fb151eb04ca1bdcfda56254
adam allalou <aabou9@gmail.com>
2021-03-23 21:54:08 +0100

    add class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable react/prefer-stateless-function, react/destructuring-assignment,
+react/no-access-state-in-setstate, no-unused-vars */
+import React, { Component } from 'react';
 import './App.css';
 import calculate from '../logic/calculate';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-
-      <ButtonPanel />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
